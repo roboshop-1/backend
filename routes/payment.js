83cf9ca99665e2a2a507aef59cf7bf89f6e85572
unknown <ghofran.bentk@gmail.com>
2022-05-20 19:57:30 +0100

    payment2

--- routes/payment.js
+++ routes/payment.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const Visa = require("../models/visa");
+const Visa = require("../models/visa_card");
 const path  =require("path");
 const bodyParser = require('body-parser');
  //---------------------------------------------
@@ -9,22 +9,36 @@ const bodyParser = require('body-parser');
  //--------------------- check if card exist --------------------------
  router.post('/payment', (req, res) => {
     console.log('Here in payment part',req.body);
-    Visa.findOne({ card : req.body.num_cart, cvv : req.body.cvv}).then(
+    const order = new Visa ({
+        card : req.body.num_cart,
+        cvv : req.body.cvv,
+    });
+    Visa.save().then(
         (result) => {
             if (result) {
-                console.log('result in ok ');
                 res.status(200).json({
-                    check: 'true'
-                })
-            }
-            else {
-                console.log('result in noo ');
-                res.status(200).json({
-                    check: 'false'
+                    message: "Order added with succes ",
+                    product: result
                 })
             }
         }
     )
+    // Visa.({ card : req.body.num_cart, cvv : req.body.cvv}).then(
+        // (result) => {
+            // if (result) {
+                // console.log('result in ok ');
+                // res.status(200).json({
+                    // check: 'true'
+                // })
+            // }
+            // else {
+                // console.log('result in noo ');
+                // res.status(200).json({
+                    // check: 'false'
+                // })
+            // }
+        // }
+    // )
 });
 
 module.exports = router;
