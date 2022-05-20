03f14858c77330fb42f24440bd849a2d743d3912
unknown <ghofran.bentk@gmail.com>
2022-05-20 19:26:31 +0100

    payment2

--- routes/payment.js
+++ routes/payment.js
@@ -8,8 +8,8 @@ const bodyParser = require('body-parser');
 
  //--------------------- check if card exist --------------------------
  router.post('/payment', (req, res) => {
-    console.log('Here in payment part');
-    Visa.findOne({ card: req.body.num_cart, cvv :req.body.cvv}).then(
+    console.log('Here in payment part',req.body);
+    Visa.findOne({ card : req.body.num_cart, cvv : req.body.cvv}).then(
         (result) => {
             if (result) {
                 res.status(200).json({
